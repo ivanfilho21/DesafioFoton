e31cba0ea43392d127f4f74a4d0e738c91c4870c
Ivan Filho <ivanfilho21@gmail.com>
2021-05-17 11:52:08 -0300

    separar papéis da Activity

--- app/src/main/java/com/example/desafiofoton/DetailsActivity.kt
+++ app/src/main/java/com/example/desafiofoton/DetailsActivity.kt
@@ -1,33 +1,25 @@
 package com.example.desafiofoton
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.example.desafiofoton.interfaces.Endpoint
 import com.example.desafiofoton.models.Movie
+import com.example.desafiofoton.repository.MovieRepository
 import com.example.desafiofoton.utils.NetworkUtils
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.desafiofoton.viewmodel.MovieViewModel
+import com.example.desafiofoton.viewmodel.MovieViewModelFactory
 
 class DetailsActivity : AppCompatActivity() {
-
-    private var movieId = 0
-    private val tag = "DetailsActivity"
+    private lateinit var viewModel : MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val title : TextView = findViewById(R.id.title)
-        val poster : ImageView = findViewById(R.id.poster)
-        val overview : TextView = findViewById(R.id.overview)
-        val genre : TextView = findViewById(R.id.genre)
-        val runtime : TextView = findViewById(R.id.runtime)
         findViewById<View>(R.id.back_button).setOnClickListener {
             finish()
         }
@@ -36,38 +28,39 @@ class DetailsActivity : AppCompatActivity() {
             finish()
         }
 
-        movieId = intent.getIntExtra("movieId", 0)
+        viewModel = ViewModelProvider(
+            this,
+            MovieViewModelFactory(
+                intent.getIntExtra("movieId", 0), MovieRepository()
+            )
+        ).get(MovieViewModel::class.java)
 
-        val client = NetworkUtils.getRetrofitInstance("https://api.themoviedb.org")
-        val endpoint = client.create(Endpoint::class.java)
-        val callback = endpoint.getMovie(movieId)
-
-        callback.enqueue(object : Callback<Movie> {
-            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
-                if (response.isSuccessful){
-                    val movie = response.body() ?: return
-                    Log.d(tag, "$movie.title")
-
-                    setTitle(movie.title)
-
-                    title.text = movie.title
-                    overview.text = movie.overview
-
-                    if (movie.genres.isNotEmpty()) {
-                        genre.text = movie.genres[0]?.name
-                    }
-
-                    runtime.text = getString(R.string.minutes, movie.runtime)
-
-                    Glide.with(this@DetailsActivity)
-                        .load(NetworkUtils.getImageUrl(movie.backdropPath, "500"))
-                        .centerCrop()
-                        .into(poster)
-                }
-            }
-            override fun onFailure(call: Call<Movie>, t: Throwable) {
-                Log.e(tag, "${t.message}")
-            }
+        viewModel.movie.observe(this, { movie ->
+            updateUI(movie)
         })
+    }
+
+    private fun updateUI(movie: Movie) {
+        val title : TextView = findViewById(R.id.title)
+        val poster : ImageView = findViewById(R.id.poster)
+        val overview : TextView = findViewById(R.id.overview)
+        val genre : TextView = findViewById(R.id.genre)
+        val runtime : TextView = findViewById(R.id.runtime)
+
+        setTitle(movie.title)
+
+        title.text = movie.title
+        overview.text = movie.overview
+
+        if (movie.genres.isNotEmpty()) {
+            genre.text = movie.genres[0]?.name
+        }
+
+        runtime.text = getString(R.string.minutes, movie.runtime)
+
+        Glide.with(this@DetailsActivity)
+            .load(NetworkUtils.getImageUrl(movie.backdropPath, "500"))
+            .centerCrop()
+            .into(poster)
     }
 }
