cbb3e02a1eae16b2ac2ab50b6276d11450d0795e
Ivan Filho <ivanfilho21@gmail.com>
2021-05-28 08:57:54 -0300

    adicionar método de retornar filme individual

--- app/src/main/java/com/example/desafiofoton/repository/MovieRepositoryInterface.kt
+++ app/src/main/java/com/example/desafiofoton/repository/MovieRepositoryInterface.kt
@@ -1,5 +1,6 @@
 package com.example.desafiofoton.repository
 
 interface MovieRepositoryInterface {
+    fun get(page: Int, callback: (result: MovieRepositoryResult) -> Unit)
     fun getPopular(page: Int, callback: (result: MovieRepositoryResult) -> Unit)
 }
