f84baaf1beb643f59ee3c9361cd5e3e2c848378a
Ivan Filho <ivanfilho21@gmail.com>
2021-05-28 08:59:42 -0300

    refatorar

--- app/src/main/java/com/example/desafiofoton/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/desafiofoton/viewmodel/MovieViewModel.kt
@@ -1,19 +1,14 @@
 package com.example.desafiofoton.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.desafiofoton.models.Movie
-import com.example.desafiofoton.repository.MovieRepository
+import com.example.desafiofoton.repository.MovieRepositoryInterface
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MovieViewModel(id : Int, repository: MovieRepository) : ViewModel() {
-    private val tag = "MovieViewModel"
+class MovieViewModel(id : Int, repository: MovieRepositoryInterface) : ViewModel() {
     private val _id = id
     private val _repository = repository
     private val _movie = MutableLiveData<Movie>()
@@ -21,26 +16,20 @@ class MovieViewModel(id : Int, repository: MovieRepository) : ViewModel() {
 
     init {
         viewModelScope.launch {
-            updateMovie()
+            fetchMovie()
         }
     }
 
-    fun updateMovie() {
-        _repository.get(_id).enqueue(object : Callback<Movie> {
-            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
-                movie.value = response.body() ?: return
-            }
-
-            override fun onFailure(call: Call<Movie>, t: Throwable) {
-                Log.e(tag, "Error loading movie.")
-            }
-        })
+    fun fetchMovie() {
+        _repository.get(_id) {
+            movie.value = it.result.results[0]
+        }
     }
 }
 
 class MovieViewModelFactory(
     private val id : Int,
-    private val repository: MovieRepository
+    private val repository: MovieRepositoryInterface
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
